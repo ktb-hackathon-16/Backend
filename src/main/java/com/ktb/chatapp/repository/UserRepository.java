ab7f290929c4ce47914e6922f86fa9902dea0173
LunaXion00 <lunaof12@gmail.com>
2026-08-10 22:36:29 +0900

    add: projection 추가

--- src/main/java/com/ktb/chatapp/repository/UserRepository.java
+++ src/main/java/com/ktb/chatapp/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.ktb.chatapp.repository;
 
 import com.ktb.chatapp.model.User;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
+
+    @Query(value = "{ '_id': { '$in': ?0 } }",
+            fields = "{ '_id': 1, 'name': 1, 'email': 1, 'profileImage': 1 }")
+    List<User> findSummariesByIdIn(Collection<String> userIds);
 }
